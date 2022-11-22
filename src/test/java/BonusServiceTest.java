dfa8f53b65be3ae58723c2f6a3ceb6365a349662
Sergey DR <svd0490@gmail.com>
2022-11-22 13:38:48 +0500

    initcommit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import ru.netology.BonusService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource (files = "src/test/resources/data.csv")
+    void shouldCalculateForRegisteredAndUnderLimit(int expected, int amount, boolean registered) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+}
